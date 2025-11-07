0680b1f2fd053617fa6a569790fc7135b10ca8be
zmomz <zmzeromz@gmail.com>
2025-11-07 22:45:05 +0300

    fix: Correct PrimeVue theme import path to lara-light-indigo

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
-import 'primevue/resources/themes/aura-light-indigo/theme.css';
+import 'primevue/resources/themes/lara-light-indigo/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
 
