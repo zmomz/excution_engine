6d6479b14f15c6d023e0a6c17234012c46f7fc43
zmomz <zmzeromz@gmail.com>
2025-11-07 22:47:13 +0300

    fix: Update PrimeVue theme to use @primevue/themes package

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
-import 'primevue/resources/themes/lara-light-indigo/theme.css';
-import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
+import Aura from '@primevue/themes/aura';
 
 import App from './App.vue'
 import router from './router'
@@ -14,6 +13,10 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(PrimeVue);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
 
 app.mount('#app')
